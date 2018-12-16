6a332a602d5805ed0688f1d23ca495711ddafb05
khalid hassan <abuuanas30@hotmail.com>
2018-12-16 15:46:57 +0200

    [ft-user-autherization#162665391] finishes login and registration

---
+++ app/api/v1/models/users.py
@@ -0,0 +1,41 @@
+from flask import jsonify
+from werkzeug.security import generate_password_hash, check_password_hash
+ USERS_LIST = []
+
+ class Users():
+     def put(self, name, username, email, password):
+         self.single_user = {}
+
+         email_data = [user for user in USERS_LIST if user["email"] == email]
+         username_data = [user for user in USERS_LIST if user["username"] == username]
+
+         if "message" not in email_data:
+             return {"message": "Email already exists"}
+        if "message" not in username_data:
+            return {"message": "Username already exists"}
+
+        self.single_user["name"] = name
+        self.single_user["username"] = username
+        self.single_user["email"] = email
+        self.single_user["password"] = password
+
+        USERS_LIST.append(self.single_user)
+
+        return {"message": "User with username {} added succesfully".format(username)}
+
+    def password_validation(self,username,password):
+        user = [user for user in USERS_LIST if user["username"] == username]
+        if "message" not in user:
+            valid = check_password_hash(
+                user['password'], password)
+            if valid:
+                return "True"
+            return {"message": "The password you entered is incorrect"}
+        return user
+
+    def get_single_user(self, username):
+        user = [user for user in USERS_LIST if user["username"] == username]
+        return user
+
+    def get_all_users(self):
+        return USERS_LIST
